f0743af9a5f33359efafb5edc467d0e0c400d638
EvaCristinaHigueras <149686679+EvaCristinaHigueras@users.noreply.github.com>
2024-10-14 08:31:52 -0400

    Update Login.py

--- ProgrammingPlan1/Login.py
+++ ProgrammingPlan1/Login.py
@@ -1,20 +1,23 @@
 from tkinter import *
 from tkinter import messagebox
 import sqlite3
+from os.path import exists
 
+# Code to create the table. The table will be organized by three columns: UserID, Username, and Password. The User ID is the primary key and the table autoincrements with each insertion.
+if exists('database.db') == False:
+    connection = sqlite3.connect('database.db')
+    # sql object that allows us to update and change the database one row at a time.
+    cursor = connection.cursor()
+    login_info = """CREATE TABLE login_info (
+    UserID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Username VARCHAR(20),
+    Password VARCHAR(30));"""
+    cursor.execute(login_info)
+else:
+    connection = sqlite3.connect('database.db')
+    # sql object that allows us to update and change the database one row at a time.
+    cursor = connection.cursor()
 
-connection = sqlite3.connect('database.db')
-#sql object that allows us to update and change the database one row at a time.
-cursor = connection.cursor()
-
-#Code to create the table. The table will be organized by three columns: UserID, Username, and Password. The User ID is the primary key and the table autoincrements with each insertion.
-'''login_info = """CREATE TABLE login_info (
-UserID INTEGER PRIMARY KEY AUTOINCREMENT,
-Username VARCHAR(20),
-Password VARCHAR(30));"""
-
-
-cursor.execute(login_info)'''
 #The Login class controls the GUI allowing the user to log in or sign up by creating a new username and password.
 class Login:
     LoginSuccessful = False
@@ -103,6 +106,10 @@ class Login:
         create = Button(master=frame1, text="Sign Up", font=("Arial", 14), justify=CENTER, fg="blue", bg="blue",
                         command=lambda: self.create_user(userid.get(), password.get()))
         create.grid(column=0, row=3)
+
+
+# This makes the tkinter work and the code run properly. 
+        root.mainloop()
 
 
 # This makes the tkinter work and the code run properly. 
